8d821c641eb025a9151b3a1e4547ca8524e608c1
SpktrSyx <ballandras.cindy@gmail.com>
2020-03-03 14:35:53 +0100

    contactForm 2 way binding

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
-const ContactForm = () =>
-  <Form>
+class ContactForm extends Component {
+  state = {
+    email: '',
+    message: '',
+  };
+  
+
+  handleEmailChange = (event) => {
+    const email = event.target.value;
+    this.setState({ email });
+  }
+
+  handleMessageChange = (event) => {
+    const message = event.target.value;
+    this.setState({ message });
+  }
+  
+  render = () => {
+    return (
+      <Form>
     <Form.Group controlId="formBasicEmail">
       <Form.Label>Email address</Form.Label>
-      <Form.Control type="email" placeholder="Enter email" />
+      <Form.Control type="email" placeholder="Enter email" 
+      onChange={this.handleEmailChange} value={this.state.email}/>
       <Form.Text className="text-muted">
         We'll never share your email with anyone else.
       </Form.Text>
     </Form.Group>
 
-    <Form.Group controlId="formBasicPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-    <Form.Group controlId="formBasicCheckbox">
-      <Form.Check type="checkbox" label="Check me out" />
-    </Form.Group>
-    <Button variant="primary" type="submit">
-      Submit
-    </Button>
-  </Form>
-;
+    <Form.Group controlId="exampleForm.ControlTextarea1">
+      <Form.Label>Message</Form.Label>
+        <Form.Control as="textarea" rows="3" 
+        onChange={this.handleMessageChange} value={this.state.message}
+        />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+        Submit
+        </Button>
+    </Form>
+    )
+  }
+}
 
 export default ContactForm;
